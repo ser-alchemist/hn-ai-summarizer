2734238d04db572de31b29a28a4236e1cc3d39f8
rafi <alchemist.rafi@gmail.com>
2026-03-29 22:41:43 +0600

    fetch stories fron hn api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import storyRoutes from "./routes/storyRoutes";
+import commentRoutes from "./routes/commentRoutes";
+import bookmarkRoutes from "./routes/bookmarkRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/stories", storyRoutes);
+app.use("/comments", commentRoutes);
+app.use("/bookmark", bookmarkRoutes);
+app.use("/bookmarks", bookmarkRoutes);
 
 app.get("/", (req, res) => {
     res.send("API running 🚀");
